9e2ae278cd46236dc006478af533847b70e4027f
Siarhei Kalita <siarhei.kalita@gmail.com>
2025-04-15 14:15:45 +0200

    init project

---
+++ src/main/java/com/kalita/reactive_azure/ReactiveAzureApplication.java
@@ -0,0 +1,13 @@
+package com.kalita.reactive_azure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveAzureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveAzureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kalita/reactive_azure/ReactiveAzureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kalita.reactive_azure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveAzureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
